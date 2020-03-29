1bb9c4ab3a6e52ecba568996e118b1196c283f52
unknown <nullbodysoft@gmail.com>
2020-03-29 14:02:51 +0700

    add tests

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+
+  def test_create_user_with_email_successful(self):
+    email = "todsaporn.o@jasmine.com"
+    password = "test1234"
+    user = get_user_model().objects.create_user(
+      email=email,
+      password=password
+    )
+    self.assertEqual(user.email,email)
+    self.assertTrue(user.check_password(password))
+  
+  def test_new_user_normalizer(self):
+    email = 'todsaporn.o@JASMINE.COM'
+
+    user = get_user_model().objects.create_user(email=email,password='test123')
+
+    self.assertEqual(user.email,email.lower())
+  
+  def test_new_user_invalid_email(self):
+    with self.assertRaises(ValueError):
+      get_user_model().objects.create_user(None,password='test123')
+      
